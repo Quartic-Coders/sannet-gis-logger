5b2a004d8bbdf2c7eb21bf2cf3fb1d11fc280c4b
andrewquartic <andrew@quarticsolutions.com>
2017-09-22 18:20:33 -0700

    upgraded logger version

--- Setup.py
+++ Setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name = "SannetLogger",
-    version = "1.0.4",
+    version = "1.0.5",
     author = "Andrew Tangeman",
     author_email = "andrew@quarticsolutions.com",
     description = ("Standardize logging module for City of San Diego batch Processing."),
