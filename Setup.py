0ca70257005227d0975b014dc07ce0fbba771cf0
andrew tangeman <andrew@quarticsolutions.com>
2017-09-23 23:36:05 -0700

    version adjust

--- Setup.py
+++ Setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name = "SannetLogger",
-    version = "1.0.5",
+    version = "1.0.7",
     author = "Andrew Tangeman",
     author_email = "andrew@quarticsolutions.com",
     description = ("Standardize logging module for City of San Diego batch Processing."),
